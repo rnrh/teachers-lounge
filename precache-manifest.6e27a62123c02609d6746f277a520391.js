e2650944519a4680c9b95236cb783b50f31946a3
rnrh <rnrhdev@gmail.com>
2019-08-12 18:42:36 -0500

    Updates

---
+++ precache-manifest.6e27a62123c02609d6746f277a520391.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2b09b82e7a693087eea",
+    "url": "/teachers-lounge-soundboard/static/css/main.434c955d.chunk.css"
+  },
+  {
+    "revision": "a2b09b82e7a693087eea",
+    "url": "/teachers-lounge-soundboard/static/js/main.a2b09b82.chunk.js"
+  },
+  {
+    "revision": "b3051add5912e07ee7a7",
+    "url": "/teachers-lounge-soundboard/static/js/1.b3051add.chunk.js"
+  },
+  {
+    "revision": "718bcd8f4c00f85ff9c4",
+    "url": "/teachers-lounge-soundboard/static/js/runtime~main.718bcd8f.js"
+  },
+  {
+    "revision": "f6090cf46450653555087dd77ef7709f",
+    "url": "/teachers-lounge-soundboard/static/media/sam-weatherman-fuck-pad.f6090cf4.m4a"
+  },
+  {
+    "revision": "24c9d2665ebb8bbf2a4bfa689e73a8bf",
+    "url": "/teachers-lounge-soundboard/static/media/bill-cravey-jealous.24c9d266.m4a"
+  },
+  {
+    "revision": "68ddf2c7480e1f24fd54e9839a5251e0",
+    "url": "/teachers-lounge-soundboard/static/media/todd-michael-jackson.68ddf2c7.m4a"
+  },
+  {
+    "revision": "94aed85e0d638512a3ad70b26b99edee",
+    "url": "/teachers-lounge-soundboard/static/media/sam-weatherman.94aed85e.png"
+  },
+  {
+    "revision": "3be6f2a329e252f0bb310aacd7df665f",
+    "url": "/teachers-lounge-soundboard/static/media/todd-padre.3be6f2a3.png"
+  },
+  {
+    "revision": "f8f6a5c5e56a3b5a408fa2ce369fe461",
+    "url": "/teachers-lounge-soundboard/static/media/bill-cravey.f8f6a5c5.png"
+  },
+  {
+    "revision": "dc52d7b5c9397c14a6d4816437300da0",
+    "url": "/teachers-lounge-soundboard/static/media/howard-levis.dc52d7b5.png"
+  },
+  {
+    "revision": "43e5b536324f3ff9809c9f076410e1ed",
+    "url": "/teachers-lounge-soundboard/static/media/tl-logo.43e5b536.png"
+  },
+  {
+    "revision": "086d008fecb596e1fa5cf59fd15fd71b",
+    "url": "/teachers-lounge-soundboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.a5de93ab1aada984774b55ab50f8f3e3.js"
+  "/teachers-lounge-soundboard/precache-manifest.6e27a62123c02609d6746f277a520391.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/teachers-lounge-soundboard/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
