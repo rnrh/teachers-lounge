1e97cab734fffccee85da1828145259145e07d93
rnrh <rnrhdev@gmail.com>
2019-09-10 12:31:28 -0500

    Updates

---
+++ precache-manifest.adcbb8287d91da8bcafc1ba46e095ac9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b98aecf7e0d0cb895c31dafe8ed6c6e",
+    "url": "/teachers-lounge/static/media/crafty-freakout.1b98aecf.mp3"
+  },
+  {
+    "revision": "7324b7681869a320a400",
+    "url": "/teachers-lounge/static/css/main.fecf3afc.chunk.css"
+  },
+  {
+    "revision": "b092c199bd7657af9e09",
+    "url": "/teachers-lounge/static/js/1.b092c199.chunk.js"
+  },
+  {
+    "revision": "0a8565018e4275bfefcc",
+    "url": "/teachers-lounge/static/js/runtime~main.0a856501.js"
+  },
+  {
+    "revision": "24c9d2665ebb8bbf2a4bfa689e73a8bf",
+    "url": "/teachers-lounge/static/media/bill-cravey-jealous.24c9d266.m4a"
+  },
+  {
+    "revision": "68ddf2c7480e1f24fd54e9839a5251e0",
+    "url": "/teachers-lounge/static/media/todd-michael-jackson.68ddf2c7.m4a"
+  },
+  {
+    "revision": "f6090cf46450653555087dd77ef7709f",
+    "url": "/teachers-lounge/static/media/sam-weatherman-fuck-pad.f6090cf4.m4a"
+  },
+  {
+    "revision": "703debc67324589306d45b1b0fc52440",
+    "url": "/teachers-lounge/static/media/howard-semetic.703debc6.m4a"
+  },
+  {
+    "revision": "7324b7681869a320a400",
+    "url": "/teachers-lounge/static/js/main.7324b768.chunk.js"
+  },
+  {
+    "revision": "8b6edaf96fa96cb9e8b86037722f3f11",
+    "url": "/teachers-lounge/static/media/todds-play.8b6edaf9.mp3"
+  },
+  {
+    "revision": "156ba6998187e18a9ffad59b7002b1c6",
+    "url": "/teachers-lounge/static/media/teachers-lounge-theme.156ba699.m4a"
+  },
+  {
+    "revision": "3672caa586728f2687de2cdfa3d5b0b8",
+    "url": "/teachers-lounge/static/media/bill-cravey.3672caa5.png"
+  },
+  {
+    "revision": "178fe1b77d09797dd3223fa35ff0120b",
+    "url": "/teachers-lounge/static/media/howard-levis.178fe1b7.png"
+  },
+  {
+    "revision": "4c4cc3b060661802c8c36ed6ecc65b35",
+    "url": "/teachers-lounge/static/media/todd-padre.4c4cc3b0.png"
+  },
+  {
+    "revision": "14c1bab9f186097598a9053479ebb579",
+    "url": "/teachers-lounge/static/media/sam-weatherman.14c1bab9.png"
+  },
+  {
+    "revision": "58d37bfba51ca233d1b6e09e66408998",
+    "url": "/teachers-lounge/index.html"
+  }
+];
